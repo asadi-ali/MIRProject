ec3018bf4b9c4b729a44175a613db3eeff40462f
Ali <asadia1376@gmail.com>
2019-11-29 19:23:03 +0330

    Add spell correction

--- services/spell_correction.py
+++ services/spell_correction.py
@@ -6,24 +6,20 @@ from .index import positional_indexer
 
 
 def get_corrected_word(word):
-    if positional_indexer.get_posting_list(word):
-        return word
-
     if word[0] in string.ascii_letters:
         stemmer = nltk.PorterStemmer()
     else:
         stemmer = hazm.Stemmer()
     word = stemmer.stem(word)
 
-    print(word)
+    if word in positional_indexer.inverted_index:
+        return word
 
     good_words = []
     for w in positional_indexer.get_all_words():
         if nltk.jaccard_distance(set(nltk.ngrams(w, n=2)),
-                                 set(nltk.ngrams(word, n=2))) > 0.7:
+                                 set(nltk.ngrams(word, n=2))) < 0.3:
             good_words.append(w)
-
-    print('کرد#کن' in good_words)
 
     best_word = '###########################'
     for w in good_words:
