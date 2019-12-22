4f8ddd6e184fc7e119e7b8291e08ac336c2b13d0
SeyedParsa <seyedparsamirtaheri@gmail.com>
2019-12-22 20:33:07 +0330

    load data and get trained classifier

--- services/classify.py
+++ services/classify.py
@@ -1,23 +1,41 @@
 import pandas as pd
+import numpy as np
+from .vectorspace import doc_to_vec
 from sklearn.ensemble import RandomForestClassifier
+from .document_manager import load_texts_and_tags, process_english_document
 from sklearn.model_selection import train_test_split
+from scipy.sparse import csr_matrix
 
+def get_trained_classifier(X_train, y_train, classifier_type):
+    if classifier_type == 'random_forest':
+        print("Training random forest on training data")
+        clf = RandomForestClassifier(n_estimators=20)
+        clf.fit(X_train, y_train)
+        print("Training completed")
+        return clf
+    else:
+        raise NotImplementedError
 
-def get_random_forest_classifier(X_train, y_train):
-    clf = RandomForestClassifier() # TODO
-    clf.fit(X_train, y_train)
-    return clf
+def one_hot(tag, tags_cnt):
+    res = np.zeros(tags_cnt)
+    res[tag] = 1
+    return res
 
-
-def load_data(path):
+def load_labeled_data(path):
+    print("Loading data from ", path)
     """
-    :param path: path to data  
-    :return: X, yg as numpy arrays
+    :param path: data file address 
+    :return: X, y data and its label
     """
-    data = pd.read_csv(path)
-    print(data)
+    texts, tags = load_texts_and_tags(path)
+    X = []
+    y = []
+    tags_cnt = max(tags)
+    for text, tag in zip(texts, tags):
+        y.append(one_hot(tag-1, tags_cnt))
+        X.append(doc_to_vec(process_english_document(text), {'tf':'n', 'idf':'t', 'norm':'n'}))
+    print("Data loading completed")
+    return np.asarray(X), np.asarray(y)
 
-
-X_train, y_train = load_data('../data/phase2_train.csv')
-X_test, y_test = load_data('../data/phase2_test.csv')
-random_forest_classifier = get_random_forest_classifier(X_train, y_train)
+def test_classifier():
+    pass
