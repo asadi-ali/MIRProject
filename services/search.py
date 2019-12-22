80abc3cbe64eaf03bc2392f41307e74975b07548
SeyedParsa <seyedparsamirtaheri@gmail.com>
2019-12-22 20:35:27 +0330

    move some methods to vectorspace

--- services/search.py
+++ services/search.py
@@ -3,39 +3,20 @@ from collections import Counter, defaultdict
 from math import log, sqrt
 from services.document_manager import document_base, raw_document_base
 from functools import lru_cache
-
-def get_tf(freq, type):
-    if type == 'b':
-        return 1 if freq > 0 else 0
-    elif type == 'n':
-        return freq
-    elif type == 'l':
-        return 1 + log(freq) if freq > 0 else 0
-    else:
-        raise NotImplementedError()
-
-def get_idf(term, type):
-    if not positional_indexer.inverted_index[term]:
-        return 0
-    if type == 'n':
-        return 1
-    elif type == 't':
-        return log(len(document_base)/len(positional_indexer.get_posting_list(term)))
-    else:
-        raise NotImplementedError()
+from .vectorspace import get_idf, tf_prime
 
 def get_related_documents(query, k, candidates = None):
     terms = set(query)
     score = defaultdict(int)
     for t in terms:
-        tf_q = get_tf(query.count(t), 'l')
+        tf_q = tf_prime(query.count(t), 'l')
         idf_q = get_idf(t, 't')
         w_q = tf_q*idf_q
         posting_list = positional_indexer.get_posting_list(t)
         for d, pos in posting_list:
             if candidates is not None and not d in candidates:
                 continue
-            tf_d = get_tf(len(pos), 'l')
+            tf_d = tf_prime(len(pos), 'l')
             idf_d = get_idf(t, 'n')
             w_d = tf_d*idf_d
             if not d in score:
@@ -100,7 +81,7 @@ def vector_length(doc_id):
     terms = set(document)
     res = 0
     for t in terms:
-        tf_d = get_tf(document.count(t), 'l')
+        tf_d = tf_prime(document.count(t), 'l')
         idf_d = get_idf(t, 'n')
         w_d = tf_d*idf_d
         res += w_d*w_d
