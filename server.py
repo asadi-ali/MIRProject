bd23251b93df602bc5ebfcc7fe2a6033f22fe5e4
SeyedParsa <seyedparsamirtaheri@gmail.com>
2019-12-22 20:34:52 +0330

    put learn method

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from services.document_manager import import_documents
 from services.index import positional_indexer, bigram_indexer
 from functions import name_to_function_mapping
+from services.classify import *
 
 
 def initialize():
@@ -20,3 +21,11 @@ def serve():
             name_to_function_mapping[method](*args)
     except KeyboardInterrupt:
         print("The program is terminated.")
+
+
+def learn():
+    X_train, y_train = load_labeled_data('data/phase2_train.csv')
+    clf = get_trained_classifier(X_train, y_train, 'random_forest')
+    X_test, y_test = load_labeled_data('data/phase2_test.csv')
+    pred = clf.predict_proba(X_test)
+    print(y_test[0], pred[0])
