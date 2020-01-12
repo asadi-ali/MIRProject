1db584f258ad07d00d4f2cee90508561bb5f2cdb
SeyedParsa <seyedparsamirtaheri@gmail.com>
2020-01-12 20:12:04 +0330

    test clustering

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import server
 
 if __name__ == '__main__':
     server.initialize()
-    server.learn('naive_bayes')
-    server.serve()
+    server.cluster('hierarchical', 'tf-idf')
+    # server.learn('naive_bayes')
+    # server.serve()
